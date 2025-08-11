7ad8bf920c0fccaafcc62a91856a3564f41ef177
YxY <1220459220@qq.com>
2025-08-11 20:58:03 +0800

    LCR_12

--- src/Lc25_08/Lc25_08_15/Array/PivotIndex_LCR_12.java
+++ src/Lc25_08/Lc25_08_15/Array/PivotIndex_LCR_12.java
@@ -17,19 +17,60 @@ public class PivotIndex_LCR_12 {
         System.out.println(solutionLCR12.pivotIndex(new int[]{1,2,3}));
         // 0
         System.out.println(solutionLCR12.pivotIndex(new int[]{2,1,-1}));
+        // -1
+        System.out.println(solutionLCR12.pivotIndex(new int[]{-1,-1,-1,-1,-1,-1}));
     }
 }
 
 
+// 0ms 100%
+// 把判断用减法，然后直接写if里面
 class SolutionLCR12 {
     public int pivotIndex(int[] nums) {
         int ans = -1;
-
+        int sum = 0;
+        int leftSum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (sum - nums[i] - leftSum == leftSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
         return ans;
     }
 }
 
 
+// 1ms  67%（我靠，还有神人？）
+// 有这样一个思路，先把所有元素遍历相加，然后再次遍历，
+// 和 减去 遍历到的元素i，然后除以2，看是否等于i之前的所有的和。
+/*
+class SolutionLCR12 {
+    public int pivotIndex(int[] nums) {
+        int ans = -1;
+        float sum = 0;
+        int leftSum = 0;
+        double count;
+        for (int num : nums) {
+            sum += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            count = (sum - nums[i]) / 2;
+            if (count == leftSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return ans;
+    }
+}
+*/
+
+
+// 378ms    6%
 // 1.题友的解法，嵌套循环，算左右和是否相等
 /*
 class SolutionLCR12 {
