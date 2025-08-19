de81c54601697b1f31f06a7c799442abde033bbf
YxY <1220459220@qq.com>
2025-08-19 18:47:06 +0800

    48(未解)

---
+++ src/Lc25_08/Lc25_08_20/Array/Rotate_48.java
@@ -0,0 +1,15 @@
+package Lc25_08.Lc25_08_20.Array;
+
+public class Rotate_48 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+// 看了题解，使用 上下翻转 和 对角线翻转，就可以得到答案
+class Solution48 {
+    public void rotate(int[][] matrix) {
+
+    }
+}
