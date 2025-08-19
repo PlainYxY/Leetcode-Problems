7f6a6f455089d5b0a9d7d0bd23c2eb6c189b59e8
YxY <1220459220@qq.com>
2025-08-19 22:07:18 +0800

    7(未解)

---
+++ src/Lc25_08/Lc25_08_20/String/Reverse_7.java
@@ -0,0 +1,31 @@
+package Lc25_08.Lc25_08_20.String;
+
+// [25.8.19](字符串) 整数反转
+/**
+ * <li>给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ * <li>如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+ * <li>假设环境不允许存储 64 位整数（有符号或无符号）。
+ */
+public class Reverse_7 {
+    public static void main(String[] args) {
+        Solution7 solution7 = new Solution7();
+
+        // 321
+        System.out.println(solution7.reverse(123));
+        // -321
+        System.out.println(solution7.reverse(-123));
+        // 21
+        System.out.println(solution7.reverse(120));
+        // 0
+        System.out.println(solution7.reverse(0));
+    }
+}
+
+
+//
+class Solution7 {
+    public int reverse(int x) {
+        int ans = 0;
+        return ans;
+    }
+}
