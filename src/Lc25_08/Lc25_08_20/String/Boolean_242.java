978fcb5018c98386de1ee4d1587085ed2c983cd5
YxY <1220459220@qq.com>
2025-08-21 21:43:30 +0800

    242

---
+++ src/Lc25_08/Lc25_08_20/String/Boolean_242.java
@@ -0,0 +1,37 @@
+package Lc25_08.Lc25_08_20.String;
+
+// [25.8.20](字符串) 有效字符异位词
+
+import java.util.*;
+
+/**
+ * <li>给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的 字母异位词。
+ */
+public class Boolean_242 {
+    public static void main(String[] args) {
+        Solution242 solution242 = new Solution242();
+
+        // true
+        System.out.println(solution242.isAnagram("anagram", "nagaram"));
+        // false
+        System.out.println(solution242.isAnagram("rat", "car"));
+        // false
+        System.out.println(solution242.isAnagram("ggii", "eekk"));
+    }
+}
+
+
+// 3ms  80%
+// 1.题解
+class Solution242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
