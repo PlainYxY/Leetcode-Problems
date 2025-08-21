587b56a5e0ea841361f0a803e890555585715696
YxY <1220459220@qq.com>
2025-08-21 21:43:51 +0800

    125(未解)

---
+++ src/Lc25_08/Lc25_08_20/String/IsAnagram_125.java
@@ -0,0 +1,22 @@
+package Lc25_08.Lc25_08_20.String;
+
+
+// [25.8.20](字符串) 验证回文串
+/**
+ *如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+ * 字母和数字都属于字母数字字符。
+ * 给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+ */
+public class IsAnagram_125 {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+//
+class Solution125 {
+    public boolean isPalindrome(String s) {
+        return false;
+    }
+}
