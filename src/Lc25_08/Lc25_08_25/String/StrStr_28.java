57457d3a90b2c1476f6a66327586c3dbd50ad4b0
YxY <1220459220@qq.com>
2025-08-25 15:32:33 +0800

    28(缺少KMP题解)

--- src/Lc25_08/Lc25_08_25/String/StrStr_28.java
+++ src/Lc25_08/Lc25_08_25/String/StrStr_28.java
@@ -7,14 +7,55 @@ package Lc25_08.Lc25_08_25.String;
  */
 public class StrStr_28 {
     public static void main(String[] args) {
+        Solution28 solution28 = new Solution28();
 
+        // 0
+        System.out.println(solution28.strStr("sadbutsad","sad"));
+        // -1
+        System.out.println(solution28.strStr("leetcode","leeto"));
+        // -1
+        System.out.println(solution28.strStr("mississippi","issipi"));
     }
 }
 
 
-// 1.
-class Solution {
+//3. KMP算法
+class Solution28 {
     public int strStr(String haystack, String needle) {
-        return 0;
+        return haystack.indexOf(needle);
     }
 }
+
+
+// 2.这是题解（一行代码） todo haystack.indexOf(needle),在干草堆（haystack）里找一根针（needle）
+//                         在字符串 haystack 中从头开始搜索子字符串 needle，并返回 needle 第一次出现的位置（索引）
+/*
+class Solution28 {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}*/
+
+
+// 1ms  40%
+// 1.两层循环，然后逐一对比
+/*
+class Solution28 {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+        for (int i = 0; i <= haystack.length()-needle.length(); i++) {
+            int j;
+                for (j = 0; j < needle.length(); j++) {
+                    if (needle.charAt(j) !=  haystack.charAt(j+i)) {
+                        break;
+                    }
+                }
+                if (j == needle.length()) {
+                    return i;
+                }
+            }
+        return -1;
+    }
+}*/
