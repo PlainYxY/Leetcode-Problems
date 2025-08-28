4084f2b10075a667883026ec48013dc4928d3dd4
YxY <1220459220@qq.com>
2025-08-28 18:11:28 +0800

    1929

---
+++ src/Lc25_08/Lc25_08_31/GetConcatenation_1929.java
@@ -0,0 +1,24 @@
+package Lc25_08.Lc25_08_31;
+
+// [25.8.27](数组) 数组串联
+/**
+ * <li>给你一个长度为 n 的整数数组 nums 。请你构建一个长度为 2n 的答案数组 ans ，数组下标 从 0 开始计数 ，对于所有 0 <= i < n 的 i ，满足下述所有要求：
+ * <li>ans[i] == nums[i]
+ * <li>ans[i + n] == nums[i]
+ * <li>具体而言，ans 由两个 nums 数组 串联 形成。
+ * <li>返回数组 ans 。
+ */
+public class GetConcatenation_1929 {
+    public static void main(String[] args) {
+        Solution1929 solution1929 = new Solution1929();
+
+    }
+}
+
+
+// 1.
+class Solution1929 {
+    public int[] getConcatenation(int[] nums) {
+        return  null;
+    }
+}
