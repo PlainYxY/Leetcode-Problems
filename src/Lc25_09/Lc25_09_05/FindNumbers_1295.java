f9623b355a24ba58bd19eb75d50c3710be79bd14
YxY <1220459220@qq.com>
2025-09-02 18:09:08 +0800

    1295

---
+++ src/Lc25_09/Lc25_09_05/FindNumbers_1295.java
@@ -0,0 +1,43 @@
+package Lc25_09.Lc25_09_05;
+
+public class FindNumbers_1295 {
+    public static void main(String[] args) {
+        Solution1295 solution1295 = new Solution1295();
+
+        // 2
+        System.out.println(solution1295.findNumbers(new int[]{12,345,2,6,7896}));
+        // 1
+        System.out.println(solution1295.findNumbers(new int[]{555,901,482,1771}));
+    }
+}
+
+
+class Solution1295 {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            int v = (int)Math.log10(num);
+            if ((v % 2) == 1) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
+
+
+// 2ms  40%
+// 检验长度
+/*
+class Solution1295 {
+    public int findNumbers(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            String numStr = String.valueOf(num);
+            if (numStr.length() % 2 == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}*/
