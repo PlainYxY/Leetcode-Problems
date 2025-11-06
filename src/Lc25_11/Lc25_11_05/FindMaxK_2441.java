d7596c0eca0e0e8db5716d71581912f0079f7fbf
YxY <1220459220@qq.com>
2025-11-06 16:48:46 +0800

    2441

--- src/Lc25_11/Lc25_11_05/FindMaxK_2441.java
+++ src/Lc25_11/Lc25_11_05/FindMaxK_2441.java
@@ -1,7 +1,8 @@
 package Lc25_11.Lc25_11_05;
 
-
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 // [25.11.04] 与对应负数同时存在的最大正整数
 public class FindMaxK_2441 {
@@ -18,7 +19,42 @@ public class FindMaxK_2441 {
 }
 
 
+// 3ms  96%
+/*
+class Solution2441 {
+    public int findMaxK(int[] nums) {
+        int[] ints = new int[1001];
+        int ans = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if(ints[Math.abs(nums[i])] == 0) {
+                ints[Math.abs(nums[i])] += nums[i];
+            }else if(ints[Math.abs(nums[i])] != 0 && ints[Math.abs(nums[i])]*nums[i] < 0){
+                ans = Math.max(ans, Math.abs(nums[i]));
+            }
+        }
+        return ans;
+    }
+}*/
+
+
+// 7ms  50%
+class Solution2441 {
+    public int findMaxK(int[] nums) {
+        int ans = -1;
+        Set<Integer> hashSet = new HashSet<>();
+        for (int num : nums) {
+            hashSet.add(num);
+            if (hashSet.contains(-num)) {
+                ans = Math.max(ans, Math.abs(num));
+            }
+        }
+        return ans;
+    }
+}
+
+
 // 9ms  13%
+/*
 class Solution2441 {
     public int findMaxK(int[] nums) {
         Arrays.sort(nums);
@@ -30,4 +66,4 @@ class Solution2441 {
         }
         return -1;
     }
-}
+}*/
