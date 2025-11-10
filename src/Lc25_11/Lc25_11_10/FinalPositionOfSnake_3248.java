b8275ee864032f63baa2b530f574f21068c6e2a3
YxY <1220459220@qq.com>
2025-11-10 14:44:46 +0800

    3248

--- src/Lc25_11/Lc25_11_10/FinalPositionOfSnake_3248.java
+++ src/Lc25_11/Lc25_11_10/FinalPositionOfSnake_3248.java
@@ -3,6 +3,7 @@ package Lc25_11.Lc25_11_10;
 import java.util.ArrayList;
 import java.util.List;
 
+// [25.11.07] 矩阵中的蛇
 public class FinalPositionOfSnake_3248 {
     public static void main(String[] args) {
         Solution3248 solution3248 = new Solution3248();
@@ -15,6 +16,7 @@ public class FinalPositionOfSnake_3248 {
 }
 
 
+// 3ms  80%
 class Solution3248 {
     public int finalPositionOfSnake(int n, List<String> commands) {
         int ans = 0;
@@ -22,7 +24,6 @@ class Solution3248 {
 
             // todo 将调用trim()方法处理后的字符串重新设置回列表的同一位置
             // commands.set(i, commands.get(i).trim());
-
             switch (command) {
                 case "DOWN":
                     ans += n;
