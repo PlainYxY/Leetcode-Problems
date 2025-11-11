e66a72b156c791decd1bdc6f0f327f0fa04c0da5
YxY <1220459220@qq.com>
2025-11-11 11:21:57 +0800

    2427

---
+++ src/Lc25_11/Lc25_11_15/CommonFactors_2427.java
@@ -0,0 +1,26 @@
+package Lc25_11.Lc25_11_15;
+
+public class CommonFactors_2427 {
+    public static void main(String[] args) {
+        Solution2427 solution2427 = new Solution2427();
+
+        // 4
+        System.out.println(solution2427.commonFactors(12,6));
+        // 2
+        System.out.println(solution2427.commonFactors(25,30));
+    }
+}
+
+
+class Solution2427 {
+    public int commonFactors(int a, int b) {
+        int ans = 0;
+        int temp = Math.max(a, b);
+        for (int i = 1; i <= temp; i++) {
+            if (a % i == 0 && b % i == 0) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
