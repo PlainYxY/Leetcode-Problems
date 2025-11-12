48b2a8299c83b601a8017d639da532db597a5748
YxY <1220459220@qq.com>
2025-11-12 20:42:16 +0800

    大根堆

--- src/DS/Max_Heap.java
+++ src/DS/Max_Heap.java
@@ -7,13 +7,27 @@ public class Max_Heap {
     public static void main(String[] args) {
         int[] Arr = {0,53,17,78,9,45,65,87,32};
 
-        // 排序为 大根堆
+        // 调用 大根堆
+        BuildMaxHeap(Arr);
+        System.out.println(Arrays.toString(Arr));
+        // 大根堆排序
+        HeapSort(Arr);
+        System.out.println(Arrays.toString(Arr));
+    }
+
+    /// 整个堆排序的时间复杂度为  O(n) + O(nLog2n) = O(nLog2n) 【两个相加只保留更高阶的一项】
+
+    // 建立 大根堆     建堆 的时间复杂度为 O(n)
+    static void BuildMaxHeap(int[] Arr){
         for (int i = (Arr.length/2); i > 0; i--) {
             HeadAdjust(Arr,i, Arr.length-1);
         }
         Arr[0] = 0;
-        System.out.println(Arrays.toString(Arr));
+    }
 
+
+    //大根堆排序完整逻辑    排序大根堆 的时间复杂度为  O(nLog2n)
+    static void HeapSort(int[] Arr){
         // 从大到小排序输出 大根堆（0位不算）
         for (int i = Arr.length-1; i > 0; i--) {
             Arr[0] = Arr[1];
@@ -23,8 +37,8 @@ public class Max_Heap {
             HeadAdjust(Arr,1,i-1);
         }
         Arr[0] = 0;
-        System.out.println(Arrays.toString(Arr));
     }
+
 
     // 排列为大根堆方法
     public static void HeadAdjust(int[] nums, int k, int len) {
