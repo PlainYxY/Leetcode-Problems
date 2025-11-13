20beb9a8fffd9cff101da1f06ddafc4c6e4c4817
YxY <1220459220@qq.com>
2025-11-13 23:14:58 +0800

    1046

---
+++ src/Lc25_11/Lc25_11_15/LastStoneWeight_1046.java
@@ -0,0 +1,17 @@
+package Lc25_11.Lc25_11_15;
+
+public class LastStoneWeight_1046 {
+    public static void main(String[] args) {
+
+        LastStoneWeight_1046 lastStoneWeight1046 = new LastStoneWeight_1046();
+
+        // 1
+        int ans = lastStoneWeight1046.lastStoneWeight(new int[]{2, 7, 4, 1, 8, 1});
+        System.out.println(ans);
+    }
+
+    public int lastStoneWeight(int[] stones) {
+
+        return 0;
+    }
+}
