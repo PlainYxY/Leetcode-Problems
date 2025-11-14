daa4bdfed534beb31a3668f2380106e84fc0864d
YxY <1220459220@qq.com>
2025-11-14 20:38:07 +0800

    1046

--- src/Lc25_11/Lc25_11_15/LastStoneWeight_1046.java
+++ src/Lc25_11/Lc25_11_15/LastStoneWeight_1046.java
@@ -1,5 +1,8 @@
 package Lc25_11.Lc25_11_15;
 
+import java.util.PriorityQueue;
+
+// [25.11.14] 最后一块石头的重量
 public class LastStoneWeight_1046 {
     public static void main(String[] args) {
 
@@ -10,8 +13,21 @@ public class LastStoneWeight_1046 {
         System.out.println(ans);
     }
 
-    public int lastStoneWeight(int[] stones) {
 
-        return 0;
+    // todo  java自带的堆方法（不太会）
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+        for (int stone : stones) {
+            pq.offer(stone);
+        }
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+            if (a > b) {
+                pq.offer(a - b);
+            }
+        }
+        return pq.isEmpty() ? 0 : pq.poll();
     }
 }
